2519c3e0beb4ef59cde912ba68e316f2db42d216
KennyJr <shuhei0634@gmail.com>
2018-05-14 10:44:54 +0900

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,8 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin:true)
+            #  activated: true,
+            #  activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -12,6 +14,8 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
+              #  activated: true,
+              #  activated_at: Time.zone.now)
 end
 
 users = User.order(:created_at).take(6)
@@ -20,3 +24,10 @@ users = User.order(:created_at).take(6)
   users.each { |user| user.microposts.create!(content: content) }
 
 end
+
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
